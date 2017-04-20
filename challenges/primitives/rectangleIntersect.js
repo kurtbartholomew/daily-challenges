04e387527173787b349d903a83d917c3607eefe3
Kurt Bartholomew <kurt.bartholomew@gmail.com>
2017-04-20 09:19:43 -0700

    (feat) Added rectangle intersect problem

---
+++ challenges/primitives/rectangleIntersect.js
@@ -0,0 +1,22 @@
+/**
+ * Given two rectangles of the form:
+ * {
+ *  x: int,
+ *  y: int,
+ *  height: int,
+ *  width: int
+ * }
+ * return an rectangle representing the intersection
+ * of the two rectangles in the same form
+ *
+ * Example: x = 747
+ * Returns: true
+ * Example: x = 10010
+ * Returns: false
+ */
+
+function rectangleIntersect (rectOne, rectTwo) {
+  // TODO
+}
+
+export default rectangleIntersect
--- test/primitives.spec.js
+++ test/primitives.spec.js
@@ -4,6 +4,7 @@ import { assert } from 'chai'
 import reverseDigits from '../challenges/primitives/reverseDigits'
 import isIntPalindrome from '../challenges/primitives/integerPalindrome'
 import uniformRandomNums from '../challenges/primitives/uniformRandomNums'
+import rectangleIntersect from '../challenges/primitives/rectangleIntersect.js'
 
 describe('Primitives', function () {
   describe('Decimal Digit Reversal', function () {
@@ -22,7 +23,7 @@ describe('Primitives', function () {
   describe('Uniform Random Numbers', function () {
     var placeHolder = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     var rand = function () {
-      return Math.random()
+      return Math.floor(Math.random())
     }
 
     it('should return an equal distribution', function () {
@@ -30,6 +31,19 @@ describe('Primitives', function () {
     })
     it('should return false if int is not a palindrome', function () {
       assert.equal(uniformRandomNums(rand, 124, 186), placeHolder)
+    })
+  })
+  describe('Rectangle Intersection', function () {
+    var rectangleOne = { x: 1, y: 2, width: 10, height: 11 }
+    var rectangleTwo = { x: 1, y: 2, width: 10, height: 11 }
+    var rectangleThree = { x: 1, y: 2, width: 10, height: 11 }
+
+    it('should return an intersection if one exists', function () {
+      assert.deepEqual(rectangleIntersect(rectangleOne, rectangleTwo), { x: 1, y: 2, width: 10, height: 11 })
+    })
+
+    it('should return an object with negative values if no intersection exists', function () {
+      assert.deepEqual(rectangleIntersect(rectangleOne, rectangleThree), { x: 0, y: 0, width: -1, height: -1 })
     })
   })
 })
